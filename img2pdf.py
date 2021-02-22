87ce8433b0759af349f33fd87d87538102fb23cb
Bernhard Heinloth <heinloth@cs.fau.de>
2021-02-22 15:28:56 +0100

    extent jbig2 to pdf wrapper

---
+++ img2pdf.py
@@ -0,0 +1,209 @@
+#!/usr/bin/env python3
+# Copyright 2006 Google Inc.
+# Author: agl@imperialviolet.org (Adam Langley)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# JBIG2 Encoder/tmp/tmpke949sra/
+# https://github.com/agl/jbig2enc
+
+import sys
+import re
+import struct
+import glob
+import os
+import platform
+import subprocess
+import tempfile
+
+dpi = 75
+ispy2 = platform.python_version_tuple()[0]=='2'
+ispy3 = platform.python_version_tuple()[0]=='3'
+
+class Ref:
+  def __init__(self, x):
+    self.x = x
+
+  def get_bytes(self):
+    return b"%d 0 R" % self.x
+
+class Dict:
+  def __init__(self, values = {}):
+    self.d = {}
+    self.d.update(values)
+
+  def get_bytes(self):
+    s = [b'<< ']
+    for (x, y) in self.d.items():
+      s.append(b'/%s ' % x.encode())
+      s.append(y.encode())
+      s.append(b"\n")
+    s.append(b">>\n")
+
+    return b''.join(s)
+
+global_next_id = 1
+
+class Obj:
+  next_id = 1
+  def __init__(self, d = {}, stream = None):
+    global global_next_id
+
+    if stream is not None:
+      d['Length'] = str(len(stream))
+    self.d = Dict(d)
+    self.stream = stream
+    self.id = global_next_id
+    global_next_id += 1
+
+  def get_bytes(self):
+    s = []
+    s.append(self.d.get_bytes())
+    if self.stream is not None:
+      s.append(b'stream\n')
+      if ispy3 and isinstance(self.stream, str):
+        s.append(self.stream.encode())
+      else:
+        s.append(self.stream)
+      s.append(b'\nendstream\n')
+    s.append(b'endobj\n')
+
+    return b''.join(s)
+
+class Doc:
+  def __init__(self):
+    self.objs = []
+    self.pages = []
+
+  def add_object(self, o):
+    self.objs.append(o)
+    return o
+
+  def add_page(self, o):
+    self.pages.append(o)
+    return self.add_object(o)
+
+  def get_bytes(self):
+    a = []
+    j = [0]
+    offsets = []
+
+    def add(x):
+      a.append(x)
+      j[0] += len(x) + 1
+    add(b'%PDF-1.4')
+    for o in self.objs:
+      offsets.append(j[0])
+      add(b'%d 0 obj' % o.id)
+      add(o.get_bytes())
+    xrefstart = j[0]
+    a.append(b'xref')
+    a.append(b'0 %d' % (len(offsets) + 1))
+    a.append(b'0000000000 65535 f ')
+    for o in offsets:
+      a.append(b'%010d 00000 n ' % o)
+    a.append(b'')
+    a.append(b'trailer')
+    a.append(b'<< /Size %d\n/Root 1 0 R >>' % (len(offsets) + 1))
+    a.append(b'startxref')
+    a.append(b'%d' % xrefstart)
+    a.append(b'%%EOF')
+
+    # sys.stderr.write(str(offsets) + "\n")
+    return b'\n'.join(a)
+
+def ref(x):
+  return '%d 0 R' % x
+
+def main(symboltable='symboltable', pagefiles=glob.glob('page-*')):
+  doc = Doc()
+  doc.add_object(Obj({'Type' : '/Catalog', 'Outlines' : ref(2), 'Pages' : ref(3)}))
+  doc.add_object(Obj({'Type' : '/Outlines', 'Count': '0'}))
+  pages = Obj({'Type' : '/Pages'})
+  doc.add_object(pages)
+  symd = doc.add_object(Obj({}, open(symboltable, 'rb').read()))
+  page_objs = []
+
+  pagefiles.sort()
+  for p in pagefiles:
+    try:
+      contents = open(p, "rb").read()
+    except IOError:
+      sys.stderr.write("error reading page file %s\n"% p)
+      continue
+    (width, height,xres,yres) = struct.unpack('>IIII', contents[11:27])
+    
+    if xres==0:
+      xres=dpi
+    if yres==0:
+      yres=dpi
+
+    xobj = Obj({'Type': '/XObject', 'Subtype': '/Image', 'Width':
+        str(width), 'Height': str(height), 'ColorSpace': '/DeviceGray',
+        'BitsPerComponent': '1', 'Filter': '/JBIG2Decode', 'DecodeParms':
+        ' << /JBIG2Globals %d 0 R >>' % symd.id}, contents)
+    contents = Obj({}, 'q %f 0 0 %f 0 0 cm /Im1 Do Q' % (float(width * 72) / xres, float(height * 72) / yres))
+    resources = Obj({'ProcSet': '[/PDF /ImageB]',
+        'XObject': '<< /Im1 %d 0 R >>' % xobj.id})
+    page = Obj({'Type': '/Page', 'Parent': '3 0 R',
+        'MediaBox': '[ 0 0 %f %f ]' % (float(width * 72) / xres, float(height * 72) / yres),
+        'Contents': ref(contents.id),
+        'Resources': ref(resources.id)})
+    [doc.add_object(x) for x in [xobj, contents, resources, page]]
+    page_objs.append(page)
+
+    pages.d.d['Count'] = str(len(page_objs))
+    pages.d.d['Kids'] = '[' + ' '.join([ref(x.id) for x in page_objs]) + ']'
+
+  doc_bytes = doc.get_bytes()
+  if ispy2:
+    print(doc_bytes)
+  elif ispy3:
+    sys.stdout.buffer.write(doc_bytes)
+  else:
+    raise Exception("unexpected python version: %s" % platform.python_version_tuple()[0])
+
+
+def usage(script, msg):
+  if msg:
+    sys.stderr.write("%s: %s\n"% (script, msg))
+  sys.stderr.write("Usage: %s [file_basename] > out.pdf\n"% script)
+  sys.exit(1)
+
+
+if __name__ == '__main__':
+  jbig2 = os.path.dirname(os.path.abspath(__file__)) + '/jbig2enc/src/jbig2'
+  if len(sys.argv) <= 2 or not sys.argv[1].isnumeric():
+    sys.stderr.write("Usage: %s [dpi] [image] [[images...]] > out.pdf\n" % sys.argv[0])
+    sys.exit(1)
+  elif not os.path.exists(jbig2):
+    sys.stderr.write("jbig2 not found - you have to build it!\n")
+    sys.exit(1)
+  else:
+    dpi = int(sys.argv[1])
+    with tempfile.TemporaryDirectory() as tmpdirname:
+      base = tmpdirname + '/output'
+      cmd = [ jbig2, '-s', '-4', '-p', '-b', base ]
+      cmd.extend(sys.argv[2:])
+      enc = subprocess.run(args = cmd, stdout=sys.stderr.buffer, stderr=sys.stderr.buffer)
+      sym = base + '.sym'
+      if enc.returncode != 0:
+        sys.stderr.write("jbig2 call failed...\n")
+        sys.exit(enc.returncode)
+      elif not os.path.exists(sym):
+        sys.stderr.write("jbig2 call succeeded but no output...\n")
+        sys.exit(1)
+      else:
+        pages = glob.glob(base + '.[0-9]*')
+        main(sym, pages)
+
